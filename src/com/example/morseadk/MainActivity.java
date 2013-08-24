0ceeb99f762f4982bde56010c39d3b9eb9442870
Ricardo Ogliari <ricardo@stillrocker.com>
2013-08-24 03:17:22 -0300

    Initial Commit

---
+++ src/com/example/morseadk/MainActivity.java
@@ -0,0 +1,186 @@
+package com.example.morseadk;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
+
+public class MainActivity extends Activity {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private PendingIntent mPermissionIntent;
+	private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+	private boolean mPermissionRequestPending;
+	private UsbManager mUsbManager;
+	private UsbAccessory mAccessory;
+	private ParcelFileDescriptor mFileDescriptor;
+	private FileInputStream mInputStream;
+	private FileOutputStream mOutputStream;
+	private TextView tvEdt;
+	private Button btn;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mUsbManager = UsbManager.getInstance(this);
+		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+				ACTION_USB_PERMISSION), 0);
+		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+		registerReceiver(mUsbReceiver, filter);
+		
+		tvEdt = (TextView) findViewById(R.id.edt);
+		btn = (Button) findViewById(R.id.btn);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (mInputStream != null && mOutputStream != null) {
+			return;
+		}
+		UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+		if (accessory != null) {
+			if (mUsbManager.hasPermission(accessory)) {
+				openAccessory(accessory);
+			} else {
+				synchronized (mUsbReceiver) {
+					if (!mPermissionRequestPending) {
+						mUsbManager.requestPermission(accessory,
+								mPermissionIntent);
+						mPermissionRequestPending = true;
+					}
+				}
+			}
+		} else {
+			Log.d(TAG, "mAccessory is null");
+		}
+	}
+
+	/** Called when the activity is paused by the system. */
+	@Override
+	public void onPause() {
+		super.onPause();
+		closeAccessory();
+	}
+
+	/**
+	 * Called when the activity is no longer needed prior to being removed from
+	 * the activity stack.
+	 */
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(mUsbReceiver);
+	}
+
+	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			if (ACTION_USB_PERMISSION.equals(action)) {
+				synchronized (this) {
+					UsbAccessory accessory = UsbManager.getAccessory(intent);
+					if (intent.getBooleanExtra(
+							UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+						openAccessory(accessory);
+					} else {
+					}
+					mPermissionRequestPending = false;
+				}
+			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+				UsbAccessory accessory = UsbManager.getAccessory(intent);
+				if (accessory != null && accessory.equals(mAccessory)) {
+					closeAccessory();
+				}
+			}
+		}
+	};
+
+	private void openAccessory(UsbAccessory accessory) {
+		mFileDescriptor = mUsbManager.openAccessory(accessory);
+		if (mFileDescriptor != null) {
+			mAccessory = accessory;
+			FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+			mInputStream = new FileInputStream(fd);
+			mOutputStream = new FileOutputStream(fd);
+			Thread thread = new Thread(null, commRunnable, TAG);
+			thread.start();
+			Log.d(TAG, "accessory opened");
+		} else {
+			Log.d(TAG, "accessory open fail");
+		}
+	}
+
+	private void closeAccessory() {
+		try {
+			if (mFileDescriptor != null) {
+				mFileDescriptor.close();
+			}
+		} catch (IOException e) {
+		} finally {
+			mFileDescriptor = null;
+			mAccessory = null;
+		}
+	}
+
+	Runnable commRunnable = new Runnable() {
+		@Override
+		public void run() {
+			int ret = 0;
+			byte[] buffer = new byte[255];
+			while (ret >= 0) {
+				try {
+					ret = mInputStream.read(buffer);
+				} catch (IOException e) {
+					break;
+				}
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						tvEdt.setEnabled(true);
+						btn.setEnabled(true);
+					}
+				});
+			}
+		}
+	};
+
+	public void showMorse(View v) {
+		String texto = tvEdt.getText().toString();
+		Toast.makeText(this, texto, 8000).show();
+		byte[] buffer = new byte[2 + texto.length()];
+		buffer[0] = 0xf;
+		buffer[1] = (byte)texto.length();
+		for (int pos = 2, i = 0; i < texto.length(); i++, pos++){
+			buffer[pos] = (byte)texto.charAt(i);
+		
+		}
+		try {
+			mOutputStream.write(buffer);
+			tvEdt.setEnabled(false);
+			btn.setEnabled(false);
+		} catch (IOException e) {}
+	}
+
+}
